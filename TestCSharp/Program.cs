325654d3525beff7f28c3a4ac7a2ecb092f128f6
xiaobaxiang <xiaobaxiang@qq.com>
2016-07-22 19:08:43 +0800

    测试项目

---
+++ TestCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ss = Console.ReadLine();
+            string bb = doSomething(ss);
+            Console.WriteLine(ss);
+            Console.WriteLine(bb);
+            Console.ReadKey();
+        }
+
+        public static string doSomething(string a)
+        {
+            a = a + "112233";
+            return a;
+        }
+    }
+}
